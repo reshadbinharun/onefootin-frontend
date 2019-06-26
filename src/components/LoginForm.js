8f9cc2c9b6bbad944abde83609613ca16258b568
reshadbinharun <md.bin_harun@tufts.edu>
2019-06-26 10:18:33 -0400

    used cors proxy console 1

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -66,6 +66,7 @@ export default class LoginForm extends React.Component {
     handleSubmitAsMentee(e) {
         e.preventDefault();
         console.log("mentee sign in")
+        console.log(BACKEND);
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
         headers.append('Accept', 'application/json');
