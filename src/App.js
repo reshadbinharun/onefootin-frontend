ea603813639ef93b64e2c001f5667c747060bbed
reshadbinharun <md.bin_harun@tufts.edu>
2019-07-10 22:03:42 -0400

    approach one

--- src/App.js
+++ src/App.js
@@ -15,126 +15,128 @@ import SignUpMentee from './components/SignUp/SignUpMentee';
 export const BACKEND = process.env.BACKEND || 'https://onefootin-dev.herokuapp.com';
 
 const PATHS = {
-  root: "/",
-  signupMentor: "/signupMentor",
-  signupMentee: "/signupMentee",
+ root: "/",
+ signupMentor: "/signupMentor",
+ signupMentee: "/signupMentee",
+ home: "/home",
 }
 
 export default class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      isMentor: false,
-      mentorPayload: {},
-      menteePayload: {},
-      loggedIn: false,
-      testMode: false,
-    };
-    this.renderLogin = this.renderLogin.bind(this);
-    this.logout = this.logout.bind(this);
-    this.login = this.login.bind(this);
-    this.toggleTest = this.toggleTest.bind(this);
-    this.liftPayload = this.liftPayload.bind(this);
-  }
+ constructor(){
+   super();
+   this.state = {
+     isMentor: false,
+     mentorPayload: {},
+     menteePayload: {},
+     loggedIn: false,
+     testMode: false,
+   };
+   this.renderLogin = this.renderLogin.bind(this);
+   this.logout = this.logout.bind(this);
+   this.login = this.login.bind(this);
+   this.toggleTest = this.toggleTest.bind(this);
+   this.liftPayload = this.liftPayload.bind(this);
+ }
 
-  toggleTest(e) {
-    e.preventDefault();
-    this.setState({
-      testMode: !this.state.testMode
-    })
-  }
+ toggleTest(e) {
+   e.preventDefault();
+   this.setState({
+     testMode: !this.state.testMode
+   })
+ }
 
-  logout(e){
-    e.preventDefault();
-    this.setState({
-      loggedIn: false
-    })
-  }
+ logout(e){
+   e.preventDefault();
+   this.setState({
+     loggedIn: false
+   })
+ }
 
-  login(){
-    this.setState({
-      loggedIn: true
-    })
-  }
+ login(){
+   this.setState({
+     loggedIn: true
+   })
+ }
 
-  liftPayload(payload, isMentor) {
-    if (isMentor) {
-      this.setState({
-        isMentor: true,
-        mentorPayload: payload
-      })
-    } else {
-      this.setState({
-        isMentor: false,
-        menteePayload: payload
-      })
-    }
-  }
+ liftPayload(payload, isMentor) {
+   if (isMentor) {
+     this.setState({
+       isMentor: true,
+       mentorPayload: payload
+     })
+   } else {
+     this.setState({
+       isMentor: false,
+       menteePayload: payload
+     })
+   }
+ }
 
-  renderLogin() {
-    let loggedInView =
-      this.state.isMentor ? 
-        <NavBarMentor
-          payload = {this.state.mentorPayload}
-        /> : 
-        <NavBarMentee
-          payload = {this.state.menteePayload}
-        />
-    let navigation =
-    <Grid centered>
-      <Router>
-            <div>
-              <div>
-              <Grid centered rows={1}>
-                <Grid.Row left>
-                  <Button
-                  >
-                    <Link to={PATHS.signupMentor}>
-                      Sign up as Mentor
-                    </Link>
-                  </Button>
-                  <Button
-                  >
-                    <Link to={PATHS.signupMentee}>
-                      Sign up as Mentee
-                    </Link>
-                  </Button>
-                </Grid.Row>
-              </Grid>
-              </div>
-              <Divider/>
-              
-              <Switch>
-                <Route exact path={PATHS.root} render={(props) => 
-                  <LoginForm {...props} 
-                    toggleTest = {this.toggleTest}
-                    login = {this.login}
-                    liftPayload = {this.liftPayload}
-                  /> 
-                }/>
-                <Route exact path={PATHS.signupMentor} render={() => 
-                  <SignUpMentor />}/>
-                <Route exact path={PATHS.signupMentee} render={() => 
-                  <SignUpMentee /> }/>
-              </Switch>
-              
-            </div>
-          </Router>
-    </Grid>
-    
-    return this.state.loggedIn ? loggedInView : navigation;
-  }
+ renderLogin() {
+   let signUpButtons =
+   <div>
+     <div>
+       <Grid centered rows={1}>
+         <Grid.Row left>
+           <Button
+           >
+             <Link to={PATHS.signupMentor}>
+               Sign up as Mentor
+             </Link>
+           </Button>
+           <Button
+           >
+             <Link to={PATHS.signupMentee}>
+               Sign up as Mentee
+             </Link>
+           </Button>
+         </Grid.Row>
+       </Grid>
+     </div>
+     <Divider/>
+   </div>
+   return (
+<Grid centered>
+ <Router>
+ <Switch>
+     <Route exact path={PATHS.root} render={(props) =>
+       <div>
+         {signUpButtons}
+         <LoginForm {...props}
+           toggleTest = {this.toggleTest}
+           login = {this.login}
+           liftPayload = {this.liftPayload}
+         />
+       </div>
+     }/>
+     <Route exact path={PATHS.signupMentor} render={() =>
+       <SignUpMentor />}/>
+     <Route exact path={PATHS.signupMentee} render={() =>
+       <SignUpMentee /> }/>
+     <Route exact path={PATHS.home} render={() =>
+       this.state.isMentor ?
+       <NavBarMentor
+         payload = {this.state.mentorPayload}
+       /> :
+       <NavBarMentee
+         payload = {this.state.menteePayload}
+       />
+       } />
+   </Switch>
+ </Router>
+ </Grid>
+ )}
 
-  render() {
-    return (
-      <div>
-        <Header
-          loggedIn={this.state.loggedIn}
-          logout={this.logout}
-          />
-        <Container>{this.renderLogin()}</Container>
-        {/* {this.state.testMode? <Test/> : null} */}
-      </div>
-    )
-  }
+ render() {
+   return (
+     <div>
+       <Header
+         loggedIn={this.state.loggedIn}
+         logout={this.logout}
+         />
+       <Container>{this.renderLogin()}</Container>
+       {/* {this.state.testMode? <Test/> : null} */}
+     </div>
+   )
+ }
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,190 +1,191 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Form, Button, Icon, Message, Grid } from 'semantic-ui-react';
-import { BACKEND } from "../App";
+import { BACKEND, PATHS } from "../App";
+
 let fieldStyle = {
-    width: '100%',
+   width: '100%',
 }
 let messageStyle = {
-    padding: '20px',
-    margin: '10px',
+   padding: '20px',
+   margin: '10px',
 }
 
 let buttonStyle = {
-    width: '80%',
+   width: '80%',
 }
 
 export default class LoginForm extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            incorrectCredentials: false,
-            error: null,
-            menteeLoginLoading: false,
-            mentorLoginLoading: false,
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmitAsMentee = this.handleSubmitAsMentee.bind(this);
-        this.handleSubmitAsMentor = this.handleSubmitAsMentor.bind(this);
-        this.renderIncorrectCredentialsMessage = this.renderIncorrectCredentialsMessage.bind(this);
-    }
+   constructor() {
+       super();
+       this.state = {
+           email: '',
+           password: '',
+           incorrectCredentials: false,
+           error: null,
+           menteeLoginLoading: false,
+           mentorLoginLoading: false,
+       }
+       this.handleChange = this.handleChange.bind(this);
+       this.handleSubmitAsMentee = this.handleSubmitAsMentee.bind(this);
+       this.handleSubmitAsMentor = this.handleSubmitAsMentor.bind(this);
+       this.renderIncorrectCredentialsMessage = this.renderIncorrectCredentialsMessage.bind(this);
+   }
 
-    //TODO: combine both handleSubmit functions into one which takes a bool for isMentor
-    handleSubmitAsMentor(e) {
-        e.preventDefault();
-        this.setState({mentorLoginLoading: true});
-        var headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        fetch(`${BACKEND}/mentorLogin`, {
-            method: 'post',
-            credentials: 'include',
-            headers: headers,
-            body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password,
-            })
-        }).then(async res => {
-            let resolvedRes = await res;
-            if (resolvedRes.status === 400) {
-                this.setState({
-                    incorrectCredentials: true,
-                    error: resolvedRes.error ? resolvedRes.error : `Your login was unsuccessful.`,
-                },() => console.log("login rejected", resolvedRes))
-            }
-            else {
-                resolvedRes = await resolvedRes.json()
-                this.setState({
-                    incorrectCredentials: false,
-                },() => {
-                    this.props.login()
-                    this.props.liftPayload(resolvedRes, true);
-                })
-            }
-        });
-    }
+   //TODO: combine both handleSubmit functions into one which takes a bool for isMentor
+   handleSubmitAsMentor(e) {
+       e.preventDefault();
+       this.setState({mentorLoginLoading: true});
+       var headers = new Headers();
+       headers.append('Content-Type', 'application/json');
+       headers.append('Accept', 'application/json');
+       fetch(`${BACKEND}/mentorLogin`, {
+           method: 'post',
+           credentials: 'include',
+           headers: headers,
+           body: JSON.stringify({
+               email: this.state.email,
+               password: this.state.password,
+           })
+       }).then(async res => {
+           let resolvedRes = await res;
+           if (resolvedRes.status === 400) {
+               this.setState({
+                   incorrectCredentials: true,
+                   error: resolvedRes.error ? resolvedRes.error : `Your login was unsuccessful.`,
+                   mentorLoginLoading: false,
+               },() => console.log("login rejected", resolvedRes))
+           }
+           else {
+               resolvedRes = await resolvedRes.json()
+               this.setState({
+                   incorrectCredentials: false,
+               },() => {
+                   this.props.login()
+                   this.props.liftPayload(resolvedRes, true);
+               })
+           }
+       });
+   }
 
-    handleSubmitAsMentee(e) {
-        e.preventDefault();
-        this.setState({menteeLoginLoading: true});
-        var headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        fetch(`${BACKEND}/menteeLogin`, {
-            method: 'post',
-            credentials: 'include',
-            headers: headers,
-            body: JSON.stringify({
-                email: this.state.email,
-                password: this.state.password,
-            })
-        }).then(async res => {
-            let resolvedRes = await res;
-            if (resolvedRes.status === 400) {
-                this.setState({
-                    incorrectCredentials: true,
-                    error: resolvedRes.error ? resolvedRes.error : `Your login was unsuccessful.`,
-                },() => console.log("login rejected", resolvedRes))
-            }
-            else {
-                resolvedRes = await resolvedRes.json()
-                this.setState({
-                    incorrectCredentials: false,
-                },() => {
-                    this.props.login()
-                    this.props.liftPayload(resolvedRes, false);
-                })
-            }
-        });        
-    }
-    
-    handleChange(e) {
-    e.preventDefault();
-    let change = {}
-    change[e.target.name] = e.target.value
-    this.setState(change)
-    }
+   handleSubmitAsMentee(e) {
+       e.preventDefault();
+       this.setState({menteeLoginLoading: true});
+       var headers = new Headers();
+       headers.append('Content-Type', 'application/json');
+       headers.append('Accept', 'application/json');
+       fetch(`${BACKEND}/menteeLogin`, {
+           method: 'post',
+           credentials: 'include',
+           headers: headers,
+           body: JSON.stringify({
+               email: this.state.email,
+               password: this.state.password,
+           })
+       }).then(async res => {
+           let resolvedRes = await res;
+           if (resolvedRes.status === 400) {
+               this.setState({
+                   incorrectCredentials: true,
+                   error: resolvedRes.error ? resolvedRes.error : `Your login was unsuccessful.`,
+                   menteeLoginLoading: false,
+               },() => console.log("login rejected", resolvedRes))
+           }
+           else {
+               resolvedRes = await resolvedRes.json()
+               this.setState({
+                   incorrectCredentials: false,
+               },() => {
+                   this.props.login()
+                   this.props.liftPayload(resolvedRes, false);
+               })
+           }
+       });       
+   }
+  
+   handleChange(e) {
+   e.preventDefault();
+   let change = {}
+   change[e.target.name] = e.target.value
+   this.setState(change)
+   }
 
-    renderIncorrectCredentialsMessage() {
-    let messageStyle = {
-        width: '80%',
-        margin: '10px'
-    }
-    return this.state.incorrectCredentials ?
-    <Grid centered>
-        <Message
-        error
-        content={this.state.error}
-        style = {messageStyle}
-    />
-    </Grid>
-    : null
-    }
-    render() {
-    return (<div>
-        <Message
-            style= {messageStyle}
-            attached
-            centered
-            header="We've been there. We'll take you there."
-            content='Please sign in.'
-        />
-        {this.renderIncorrectCredentialsMessage()}
-            <Grid>
-            <Grid.Row centered>
-            <Form >
-                <Form.Field
-                type="email"
-                required="true"
-                style={fieldStyle}
-                >
-                    <label>Email</label>
-                    <input placeholder='Email' name="email" onChange={this.handleChange} />
-                </Form.Field>
-                <Form.Field
-                    type="password"
-                    required="true"
-                    style={fieldStyle}
-                >
-                    <label>Password</label>
-                    <input placeholder='***' name="password" onChange={this.handleChange} />
-                </Form.Field>
-                </Form>
-            </Grid.Row>
-            <Grid.Row centered columns={2}>
-                <Grid.Column>
-                    <Button
-                        style={buttonStyle}
-                        onClick={this.handleSubmitAsMentee}
-                        color="yellow" 
-                        loading={this.state.menteeLoginLoading}
-                    >
-                        <Icon name="unlock"/>
-                        Login as Mentee
-                    </Button>
-                </Grid.Column>
-                <Grid.Column>
-                    <Button 
-                        style={buttonStyle}                        
-                        onClick={this.handleSubmitAsMentor}
-                        color="orange"
-                        loading={this.state.mentorLoginLoading}
-                    >
-                        <Icon name="unlock"/>
-                        Login as Mentor
-                    </Button>
-                </Grid.Column>
-            </Grid.Row>
-
-            <Grid.Row centered>
-                <Button onClick={this.props.toggleTest}>
-                Toggle Test Mode
-                </Button>
-            </Grid.Row>
-            </Grid>
-            
-        </div>)
-    }
+   renderIncorrectCredentialsMessage() {
+   let messageStyle = {
+       width: '80%',
+       margin: '10px'
+   }
+   return this.state.incorrectCredentials ?
+   <Grid centered>
+       <Message
+       error
+       content={this.state.error}
+       style = {messageStyle}
+   />
+   </Grid>
+   : null
+   }
+   render() {
+   return (<div>
+       <Message
+           style= {messageStyle}
+           attached
+           centered
+           header="We've been there. We'll take you there."
+           content='Please sign in.'
+       />
+       {this.renderIncorrectCredentialsMessage()}
+           <Grid>
+           <Grid.Row centered>
+           <Form >
+               <Form.Field
+               type="email"
+               required="true"
+               style={fieldStyle}
+               >
+                   <label>Email</label>
+                   <input placeholder='Email' name="email" onChange={this.handleChange} />
+               </Form.Field>
+               <Form.Field
+                   type="password"
+                   required="true"
+                   style={fieldStyle}
+               >
+                   <label>Password</label>
+                   <input placeholder='***' name="password" onChange={this.handleChange} />
+               </Form.Field>
+               </Form>
+           </Grid.Row>
+           <Grid.Row centered columns={2}>
+               <Grid.Column>
+                   <Button
+                       style={buttonStyle}
+                       onClick={this.handleSubmitAsMentee}
+                       color="yellow"
+                       loading={this.state.menteeLoginLoading}
+                   >
+                       <Icon name="unlock"/>
+                       <Link to={PATHS.home}>
+                           Login as Mentee
+                       </Link>
+                   </Button>
+               </Grid.Column>
+               <Grid.Column>
+                   <Button
+                       style={buttonStyle}                       
+                       onClick={this.handleSubmitAsMentor}
+                       color="orange"
+                       loading={this.state.mentorLoginLoading}
+                   >
+                       <Icon name="unlock"/>
+                       <Link to={PATHS.home}>
+                           Login as Mentor
+                       </Link>  
+                   </Button>
+               </Grid.Column>
+           </Grid.Row>
+           </Grid>
+          
+       </div>)
+   }
 }
