b6d7c376df2b8ee82bc8a41884948f13ad9170a1
reshadbinharun <md.bin_harun@tufts.edu>
2019-07-19 19:47:16 -0400

    added audio lol

--- src/components/Video/VideoComponent.js
+++ src/components/Video/VideoComponent.js
@@ -132,6 +132,7 @@ joinRoom() {
 
     console.log("Joining room '" + this.state.roomName + "'...");
     let connectOptions = {
+        audio: true,
         name: this.state.roomName,
         video: {width: VIDEO_WIDTH}
     };
