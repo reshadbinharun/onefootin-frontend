2041f421dd01f4a91766ebf75f6d443604430953
reshadbinharun <md.bin_harun@tufts.edu>
2019-08-10 17:34:44 -0400

    handleChangeLanguages function bound

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -38,6 +38,7 @@ export default class EditProfile extends React.Component {
         this.handleChangeTopic = this.handleChangeTopic.bind(this);
         this.handleMentorEditSubmit = this.handleMentorEditSubmit.bind(this);
         this.handleMenteeEditSubmit = this.handleMenteeEditSubmit.bind(this);
+        this.handleChangeLanguages = this.handleChangeLanguages.bind(this);
     }
 
     handleMentorEditSubmit(e) {
