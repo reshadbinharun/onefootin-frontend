917ab821a74b06ce21819a1f420e37bfaa5f57ba
reshadbinharun <md.bin_harun@tufts.edu>
2019-08-22 22:03:59 -0400

    reload after setState done right

--- src/components/ScheduleFormMentorPicked.js
+++ src/components/ScheduleFormMentorPicked.js
@@ -108,7 +108,7 @@ export default class ScheduleFormMentorPicked extends Component {
                     });
                     this.setState({
                         submitting: false,
-                    }).then(() => {
+                    },() => {
                         window.location.reload();
                     });
                 }
